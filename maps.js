9f632bf37bc2e5f81d87fc4d71731d8e1816da0c
Samuel Rivas <robertbbenn@gmail.com>
2023-10-18 03:17:57 -0500

    added misterious things

---
+++ maps.js
@@ -0,0 +1,48 @@
+const emojis={
+  '-':' ',
+  'O':'ðŸšª',
+  'X':'ðŸ’£',
+  'I':'ðŸŽ',
+  'PLAYER':'☠',
+  'BOMB_COLLISION':'🎇',
+  'GAME_OVER':'👎🏼',
+  'WIN':'🏆',
+};
+
+const maps=[];
+maps.push(`
+  IXXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  OXXXXXXXXX
+`);
+maps.push(`
+  O--XXXXXXX
+  X--XXXXXXX
+  XX----XXXX
+  X--XX-XXXX
+  X-XXX--XXX
+  X-XXXX-XXX
+  XX--XX--XX
+  XX--XXX-XX
+  XXXX---IXX
+  XXXXXXXXXX
+  `);
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX-----OXX
+  XXXXXXXXXX
+`);
