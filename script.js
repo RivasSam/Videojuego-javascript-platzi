13444453243df1990c18538ac9d5bb94e724f73c
Samuel Rivas <robertbbenn@gmail.com>
2023-10-19 01:34:48 -0500

    finished class 4, resizing bug fixed

--- script.js
+++ script.js
@@ -5,30 +5,32 @@ let canvasSize;
 let elementsSize;
 
 
-window.addEventListener('load', startGame)
-window.addEventListener('resize', startGame)
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
 
-function startGame() {
-  setCanvasSize()
-
-  game.font = elementsSize+'px Verdana';
-  game.textAlign = ''
-
-   for (let i = 0; i < 10; i++) {
-      game.fillText(emojis['X'], elementsSize * i, elementsSize )
-   }
-}
 
 function setCanvasSize() {
-
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8
+    canvasSize = window.innerWidth * 0.8;
   } else {
-    canvasSize = window.innerHeight * 0.8
+    canvasSize = window.innerHeight * 0.8;
   }
-
+  
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = (canvasSize / 10) * 0.95;
+  
+  startGame();
+}
 
-  elementsSize = (canvasSize / 10) - 1;
+function startGame() {
+  console.log({canvasSize, elementsSize });
+
+  game.font = elementsSize+'px Verdana';
+  game.textAlign = 'end';
+
+   for (let i = 1; i <= 10; i++) {
+      game.fillText(emojis['X'], elementsSize + (elementsSize*0.4), elementsSize * i);
+   }
 }
